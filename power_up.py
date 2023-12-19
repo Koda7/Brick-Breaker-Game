ac4643b0379ed6542c2ff34d120ba6850c33c8f3
Ruthvik Kodati <ruthvikkodati@gmail.com>
2023-12-19 08:05:09 -0500

    Added files

--- power_up.py
+++ power_up.py
@@ -1,46 +1,46 @@
-import time
+# import time
 
 
-class Power_Up:  # parent class for the Power Ups
+# class Power_Up:  # parent class for the Power Ups
 
-    def __init__(self, x, y, symbol):  # constructor for the power ups
-        self.__x = x
-        self.__y = y
-        self.__start_time = None
-        self.__is_present = True
-        self.__caught = False
-        self.__symbol = symbol
-        self.__last_change = time.time()
+#     def __init__(self, x, y, symbol):  # constructor for the power ups
+#         self.__x = x
+#         self.__y = y
+#         self.__start_time = None
+#         self.__is_present = True
+#         self.__caught = False
+#         self.__symbol = symbol
+#         self.__last_change = time.time()
 
-    def get_coordinates(self, width):  # return the location of the power up
-        if(int((time.time() - self.__last_change) / 0.1) > 1):
-            self.__x += 1
-            self.__last_change = time.time()
-        if(self.__x == width - 1):
-            self.__is_present = False
-        return self.__x, self.__y
+#     def get_coordinates(self, width):  # return the location of the power up
+#         if(int((time.time() - self.__last_change) / 0.1) > 1):
+#             self.__x += 1
+#             self.__last_change = time.time()
+#         if(self.__x == width - 1):
+#             self.__is_present = False
+#         return self.__x, self.__y
 
-    def is_present(self):  # if the power up if present and need to be displayed
-        return self.__is_present
+#     def is_present(self):  # if the power up if present and need to be displayed
+#         return self.__is_present
 
-    def get_start_time(self):  # the time when the power up was caught
-        return self.__start_time
+#     def get_start_time(self):  # the time when the power up was caught
+#         return self.__start_time
 
-    def caught(self):  # The power up is caught by the paddle
-        self.__caught = True
-        self.__start_time = time.time()
+#     def caught(self):  # The power up is caught by the paddle
+#         self.__caught = True
+#         self.__start_time = time.time()
 
-    def is_caught(self):  # If the power up is caught by the paddle
-        return self.__caught
+#     def is_caught(self):  # If the power up is caught by the paddle
+#         return self.__caught
 
-    def symbol(self):  # The respective symbol of the power up
-        return self.__symbol
+#     def symbol(self):  # The respective symbol of the power up
+#         return self.__symbol
 
-    def finish(self):  # The power up is no longer active
-        self.__is_present = False
+#     def finish(self):  # The power up is no longer active
+#         self.__is_present = False
 
-    def execute(self):  # Execute the effect of the power up
-        pass
+#     def execute(self):  # Execute the effect of the power up
+#         pass
 
-    def reverse(self):  # Reverse the effect of the power up
-        pass
+#     def reverse(self):  # Reverse the effect of the power up
+#         pass
