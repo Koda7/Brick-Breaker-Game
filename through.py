e40c5f0868034dde00e082464302d8cbcccd3f13
Ruthvik Kodati <ruthvikkodati@gmail.com>
2023-12-20 14:03:35 -0500

    Added files

--- through.py
+++ through.py
@@ -1,13 +1,13 @@
-from power_up import Power_Up
+# from power_up import Power_Up
 
-class Through(Power_Up):  # class for the Through Ball power up
+# class Through(Power_Up):  # class for the Through Ball power up
 
-    def __init__(self, x, y):  # constructor for the class
-        super().__init__(x, y, 'T')
+#     def __init__(self, x, y):  # constructor for the class
+#         super().__init__(x, y, 'T')
 
-    def execute(self):  # execute the power up
-        return {"through": 1}
+#     def execute(self):  # execute the power up
+#         return {"through": 1}
 
-    def reverse(self):  # reverse the effects of the power up
-        self.finish()
-        return {"through": 0}
+#     def reverse(self):  # reverse the effects of the power up
+#         self.finish()
+#         return {"through": 0}
