c0519556b1c118949eb7c1632836b19de3223a95
Ruthvik Kodati <ruthvikkodati@gmail.com>
2023-12-26 08:38:34 -0500

    Added files

--- fast.py
+++ fast.py
@@ -1,14 +1,14 @@
-from power_up import Power_Up
+# from power_up import Power_Up
 
 
-class Fast(Power_Up):  # class for the Fast Ball Power Up
+# class Fast(Power_Up):  # class for the Fast Ball Power Up
 
-    def __init__(self, x, y):  # constructor for the class
-        super().__init__(x, y, 'F')
+#     def __init__(self, x, y):  # constructor for the class
+#         super().__init__(x, y, 'F')
 
-    def execute(self):  # execute the power up
-        return {"fast": 1}
+#     def execute(self):  # execute the power up
+#         return {"fast": 1}
 
-    def reverse(self):  # reverse the effects of the power up
-        self.finish()
-        return {"fast": 0}
+#     def reverse(self):  # reverse the effects of the power up
+#         self.finish()
+#         return {"fast": 0}
